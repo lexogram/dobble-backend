3a768228d5cb3c5412b1e223088022018b1274f3
James Newton <blackslate@lexogram.com>
2024-01-04 14:39:04 +0300

    Provide a reason for socket closing uncleanly
    index.html
    ---
    * Add "reason" class to #error p
    
    styles.css
    ---
    * Centre align text for #error p.reason
    
    websocket.js
    ---
    * Add look-up for standard codes
    * Modify reason for 1006 to "The server shut down unexpectedly."
    * Set textContent of p.reason appropriately.

--- client/public/js/websocket.js
+++ client/public/js/websocket.js
@@ -47,16 +47,38 @@ socket.onmessage = (event) => {
 
 
 socket.onclose = ({ wasClean, code, reason }) => {
-  // console.log("wasClean:", wasClean);
-  // console.log("code:", code);
-  // console.log("reason:", reason);
-
   if (wasClean) {
     location.hash = "login"
     focusOn(".user_name")
 
   } else {
+    if (!reason) {
+      reason = {
+        "1000": "Normal Closure",
+        "1001": "Going Away",
+        "1002": "Protocol error",
+        "1003": "Unsupported Data",
+        "1004": "Reserved",
+        "1005": "No Status Rcvd",
+        "1006": "The server shut down unexpectedly.", // "Abnormal Closure",
+        "1007": "Invalid frame payload data",
+        "1008": "Policy Violation",
+        "1009": "Message Too Big",
+        "1010": "Mandatory Ext.",
+        "1011": "Internal Error",
+        "1012": "Service Restart",
+        "1013": "Try Again Later ",
+        "1014": "Bad Gateway",
+        "1015": "TLS handshake"
+      }[code]
+
+      if (!reason) {
+        reason = "An unexpected error occured."
+      }
+    }
+
     location.hash = "error"
+    document.querySelector(":target .reason").textContent = reason
     focusOn(":target button")
   }
 }
