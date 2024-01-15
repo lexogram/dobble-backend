bd606c3190897a0db834bcb2bf962621c894e8b2
James Newton <blackslate@lexogram.com>
2024-01-15 23:19:53 +0300

    Comment cors to find what origins are allowed

--- server/utilities/cors.js
+++ server/utilities/cors.js
@@ -1,30 +1,30 @@
 /**
  * server/utilities/cors.js
- * 
+ *
  * CORS requires information on which external sites are allowed
  * to access files on this server. One way to do this is to
  * provide an array of acceptable hosts. Hosts can be defined as
  * strings, like "http://domain.name:1234" or as regular
  * expressions, like /https?:\/\/domain\.name(:\d+)?/i
- * 
+ *
  *   [ "http://domain.name:1234",
  *     /https?:\/\/domain\.name(:\d+)?/i
  *   ]
- * 
+ *
  * When reading from the .env file, backslash characters need to
  * be escaped with a backslash character (\\). The data from the
  * .env file will be read in as a string, and then processed by
  * JSON.parse(), so all regular expressions must be enclosed in
  * double-quotes.
- * 
+ *
  * String hosts will be built up from their constituent parts
  * (protocol, host, port), so no backslash characters should be
  * needed.
- * 
+ *
  *  PROTOCOL=http
  *  ORIGINS=["/localhost(:\\d+)?$/i", "127.0.0.1" ]
  *  PORTS=[3000,3001]
- * 
+ *
  * The .env entries above will allow access to any port on
  * localhost, or to sites running at http://127.0.0.1:3000 or
  * http://127.0.0.1:3001.
@@ -36,6 +36,12 @@
 const PROTOCOL = process.env.PROTOCOL || "https"
 const ORIGINS = process.env.ORIGINS
 const PORTS = process.env.PORTS
+
+console.log("PROTOCOL:", PROTOCOL);
+console.log("ORIGINS:", ORIGINS);
+console.log("PORTS:", PORTS);
+
+
 
 let originsParsed = false
 let isOnLAN = true // will be set to false if there is any doubt
@@ -49,36 +55,53 @@ const ports = (() => {
     return []
   }
 })()
-// console.log("ports:", ports);
+console.log("ports:", ports);
 
 // Define which domains are allowed...
-let origin 
+let origin
 try {
   origin = JSON.parse(ORIGINS)
-  .map( origin => {
+  console.log("parsed origin:", origin);
+
+  origin = origin.map( origin => {
     const match = /\/(.+)\/(.+)?/.exec(origin)
 
-    if (match) {    
+    if (match) {
+      console.log("match:", match);
+
       // Extract the regular expression and use it
       const [ , expression, options ] = match
+      console.log("expression:", expression);
+      console.log("options:", options);
+
+
       isOnLAN &&= isLocalHost(expression)
       origin = new RegExp(expression, options)
+      console.log("(in map) regex origin:", origin);
+
 
     } else {
       // Apply the given protocol to every string host
       isOnLAN &&= isLocalHost(origin)
       origin = `${PROTOCOL}://${origin}`
+      console.log("(in map) standard origin:", origin);
+
     }
 
     return origin
   })
+
+  console.log("mapped origin:", origin);
+
   // ... and include all the acceptable ports
-  .reduce(( origin, hostname ) => {
+  origin = origin.reduce(( origin, hostname ) => {
     origin.push(hostname) // in all cases, RegExp or raw string
 
     if (hostname instanceof RegExp || /:\d+$/.test(hostname)) {
       // Ignore Regular Expressions and entries with a port
+      console.log("RegExp simply added as is")
     } else {
+      console.log("Adding ports:", ports)
       ports.forEach( port => origin.push(`${hostname}:${port}`))
     }
 
@@ -90,11 +113,20 @@ try {
 } catch (error) {
   // Prevent all connections on a public-facing server if
   // .env is not correctly set up
+  console.log(`Caught ${error}`)
+
   origin = []
 }
 
+console.log("originsParsed:", originsParsed);
+console.log("isOnLAN:", isOnLAN);
+console.log("treated origin:", origin);
+
+
+
 if (!origin.length && originsParsed && isOnLAN) {
   // Allow all connections when working on a local network
+  console.log("setting origin safely to *")
   origin = "*"
 }
 
@@ -119,6 +151,6 @@ function isLocalHost(expression) {
   return false
 }
 
-// console.log("origin:", origin);
+console.log("module.exports =", origin);
 
 module.exports = origin
